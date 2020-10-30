41b483a61ef9a79545ccc3253e680c09ffdfe9f2
Saquib <saquibfortuitycorps@gmail.com>
2020-10-30 10:57:06 -0700

    commit both tree traversal

---
+++ tree_preorder.py
@@ -0,0 +1,22 @@
+class node:
+    def __init__(self,val):
+        self.leftchild = None
+        self.rightchild = None
+        self.nodedata = val
+
+
+root = node(1)
+root.leftchild = node(2)
+root.rightchild = node(3)
+root.leftchild.leftchild = node(4)
+root.leftchild.rightchild = node(5)
+
+def preorder(root):
+    if root:
+        print(root.nodedata)
+        preorder(root.leftchild)
+        preorder(root.rightchild)
+
+
+preorder(root)
+
