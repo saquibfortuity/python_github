e172e7899a15e42e6b73a2a29fbd504f057939df
Saquib <saquibfortuitycorps@gmail.com>
2020-11-04 14:17:44 -0800

    abc

---
+++ training_test.py
@@ -0,0 +1,23 @@
+#a = ('val1', (1, 2 ), (2.5, 5.6, 8.2))
+#print(a)
+#print(type(a))
+#print(a[1][1])
+
+b = ['gmc', 'audi', 'bmw', 'ford'], ['saquib', 'ayush', 'dan', 'maniac'], [1, 2, 3, 4], [2.3, 4.6, 8.2, 3.4]
+print(b)
+print(type(b))
+print(b[0][-1:-2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
